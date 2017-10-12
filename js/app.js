3bbb6f70975f4f1b626af87634d8897fa10644aa
wietecha-mrzyglod <wietecha.katarzyna@gmail.com>
2017-10-12 21:34:19 +0200

    Created start screen

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 $(function(){
-  console.log("Hi");
 
+  var startBox = $("#start");
+  var gameBox = $("#game");
+  var endBox = $("#end");
+  var startGameButton = $("div.pig-place");
+  var button = $("button");
 
+  startGameButton.on("click",function(){
+    startBox.css("display", "none");
+    gameBox.css("display", "block");
+  });
 
+  button.on("click", function(){
+    gameBox.css("display", "none");
+    endBox.css("display", "block");
+  })
 
 
 });
