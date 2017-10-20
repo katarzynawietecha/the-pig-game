e44c9bdbf1329df2d8d6d5eec200359bda92768b
wietecha-mrzyglod <wietecha.katarzyna@gmail.com>
2017-10-20 21:08:36 +0200

    Modified rollButton click event

--- js/app.js
+++ js/app.js
@@ -12,17 +12,18 @@ $(function(){
   var holdButton = $(".hold");
 
   //Other
-  var dice = $("img.dice");
+  var dicePicture = $("img.dice");
   var score0 = $("#score-0");
   var score1 = $("#score-1");
   var current0 = $("#current-0");
   var current1 = $("#current-1");
   var name0 = $("#name-0");
   var name1 = $("#name-1");
-  var player0Panel = $(".player-0-panel");
-  var player1Panel = $(".player-1-panel");
+  // var player0Panel = $(".player-0-panel");
+  // var player1Panel = $(".player-1-panel");
 
-  var scores, roundScore, activePlayer;
+  //Numeral
+  var diceNumber, scores, roundScore, activePlayer;
 
   //Start button
   startButton.on("click",function(){
@@ -32,29 +33,51 @@ $(function(){
   });
 
   rollButton.on("click",function(){
-    console.log("RollButton is clicked!")
+    //Roll dice and display
+    diceNumber = Math.floor(Math.random()*6)+1;
+    dicePicture.show();
+    dicePicture.attr("src", "images/dice-"+diceNumber+".png");
+
+    if( diceNumber !== 1){ //IF the number is NOT "1": update round score
+      roundScore += diceNumber;
+      if(activePlayer == 0){
+        // current0.innerText = roundScore; - it's not working
+        document.querySelector('#current-0').innerText = roundScore;
+      }else{
+        // current1.innerText = roundScore; - it's not working
+        document.querySelector('#current-1').innerText = roundScore;
+      }
+    }else{  //IF the number is "1": next player
+      nextPlayer()
+    }
+
   })
+
 
   holdButton.on("click",function(){
     console.log("HoldButton is clicked!")
   })
 
 
+  function nextPlayer(){
+    console.log("Next player!");
+  }
+
   function newGame(){
     scores = [0, 0];
     activePlayer = 0;
     roundScore = 0;
 
-    dice.hide();
+    dicePicture.hide();
     score0.innerText = "0";
     score1.innerText = "0";
     current0.innerText = "0";
     current1.innerText = "0";
     name0.innerText = "Player 1";
     name1.innerText = "Player 2";
-    player0Panel.removeClass("active");
-    player1Panel.removeClass("active");
-    player0Panel.addClass("active");
+    name0.removeClass("active");
+    name1.removeClass("active");
+    name0.addClass("active");
   }
 
 
